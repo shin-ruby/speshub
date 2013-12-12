45dccaff750fbb9174bbf46ff186cb072f7907c8
shin-ruby <shin.php@gmail.com>
2013-12-12 14:21:21 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Speshub::Application.config.secret_token = '75c7982a55054e5fb3f9cb51104759a187be30a5918df388d9aa07a7559cdab39be14ab6fdf6774b5a060d3d5ce2a2a9c79ca90d7649d41b8a0abac8167b5c52'
